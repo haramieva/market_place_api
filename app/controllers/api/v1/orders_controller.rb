8c99670eaed3584e0c6ec0801e8edbf6e8af8969
haramieva <a.haramieva@gmail.com>
2016-05-31 18:58:22 +0300

    Adds the show action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::OrdersController < ApplicationController
+
   before_action :authenticate_with_token!
+
   respond_to :json
 
   def index
-    respond_with current_user.orders
+    respond_with current_user.orders.find(params[:id])
   end
 end
--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::OrdersController, type: :controller do
+
   describe "GET #index" do
     before(:each) do
       current_user = FactoryGirl.create :user
@@ -12,6 +13,22 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
     it "returns 4 order records from the user" do
       orders_response = json_response[:orders]
       expect(orders_response).to have(4).items
+    end
+
+    it { should respond_with 200 }
+  end
+
+  describe "GET #show" do
+    before(:each) do
+      current_user = FactoryGirl.create :user
+      api_authorization_header current_user.auth_token
+      @order = FactoryGirl.create :order, user: current_user
+      get :show, user_id: current_user.id, id: @order.id
+    end
+
+    it "returns the user order record matching the id" do
+      order_response = json_response[:order]
+      expect(order_response[:id]).to eql @order.id
     end
 
     it { should respond_with 200 }
