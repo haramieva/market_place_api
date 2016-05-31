991bc55bdec58b9ecf4d3004291af0a163d0d058
haramieva <a.haramieva@gmail.com>
2016-05-31 03:39:47 +0300

    Finishes modeling the product model along with user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ActiveRecord::Base
+  validates :auth_token, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :auth_token, uniqueness: true
-
   before_create :generate_authentication_token!
+
+  has_many :products, dependent: :destroy
 
   def generate_authentication_token!
     begin
