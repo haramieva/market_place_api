09eb764ac2c41be616cf6b973c91a7cf70154617
haramieva <a.haramieva@gmail.com>
2016-05-31 04:08:25 +0300

    Adds the products create, update and destroy action nested on the user resources

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Product, type: :model do
   it { should respond_to(:price) }
   it { should respond_to(:published) }
   it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :price }
